ba27118661d6d600efd2d9347e4d6dacdf1d2b18
farhanali234 <farhanali234@gmail.com>
2021-01-13 21:16:14 +0530

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { lorem } from 'faker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'typing';
+  enteredText='';
+  randomText =lorem.sentence();
+  typed(value : string){
+    // console.log(value)
+    this.enteredText = value;
+  }
+  compare( randomLetter : string, enteredLetter:string ){
+    if(!enteredLetter){
+      return 'pending';
+    }
+    return randomLetter === enteredLetter ? 'correct' : 'incorrect';
+
+  }
+  
 }
+
